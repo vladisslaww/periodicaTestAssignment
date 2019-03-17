146686840d45a495d5430eaeca64e450717c1b61
vladisslaww <vladisslaww@gmail.com>
2019-03-17 19:09:21 +0300

    initial commit: requirements are implemented

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # periodicaTestAssignment
+
+### **Дано**
+
+На входе есть SVG разметка:
+
+```
+<svg width="584" height="584" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewport-fill="white">
+
+<defs>
+  <clipPath id="image-clip-path">
+    <rect x="178" y="178" width="228" height="228"/>
+  </clipPath>
+</defs>
+  
+<image id="image" x="178" y="178" width="228" height="228" xlink:href="https://static.periodica.press/public/photo_dpi72.jpg" preserveAspectRatio="xMidYMid slice"/>
+
+</svg>
+```
+
+### Требуется
+
+Разработать страницу на которой отображается SVG разметка и есть:
+1. поле ввода для URL на фотографию, которую требуется отобразить в разметке
+кнопка ОК.
+2. Вводим ссылку на фотографию, жмем ОК – фото должно отобразиться в SVG разметке.
+3. Если заданное по ссылке фото не квадратное, то нужно его вписать в квадрат, так, чтобы оно занимало все пространство.
+4. Можно курсором мыши позиционировать изображение в квадрате (но нельзя, чтобы изображение выходило за рамки квадрата).
+5. Добавить поле ввода “масштаб”, которое позволит увеличить фотографию (масштаб может быть от 100% - исходные размеры (см шаг 3) до 300% - трехкратное увеличение.
+
+**как это может выглядеть:** 
+https://www.dropbox.com/s/hzcwr8qi7qd1wjc/test-case-sample.mov?dl=0 
+
+**изменение масштаба:** 
+https://www.dropbox.com/s/9no5zn51yacssob/zooming.mov?dl=0 
+
---
+++ index.js
@@ -0,0 +1,169 @@
+// Create variables
+
+const image = document.getElementById('image');
+const form = document.querySelector('form');
+const urlInput = document.getElementById('url_input');
+const slider = document.getElementById('slider_range');
+
+
+let baseWidth, baseHeight, imageX, imageY, mousePosition, startPosition, sliderStart;
+
+// Object reference
+let currentWidth = image.width.baseVal;
+let currentHeight = image.height.baseVal;
+
+let currentX = image.x.baseVal;
+let currentY = image.y.baseVal;
+
+
+let isImageEvent = false;
+let isSliderEvent = false;
+
+let minVals = getMinVals();
+
+const maxVals = {
+  x: 178,
+  y: 178,
+};
+
+
+// Input value selection
+urlInput.addEventListener('focus', function (event) {
+  event.target.select();
+});
+
+// Form submission. Changes urlInput's href
+form.addEventListener('submit', function (event) {
+
+  event.preventDefault();
+
+  if (urlInput.value){
+
+    fetch(urlInput.value) // Check if it's possible to fetch the image
+      .then( (res) => {
+
+        if (res.ok) {
+
+          image.removeAttribute('width');  // Remove width and height to reset them with the new image
+          image.removeAttribute('height');
+          image.href.baseVal = urlInput.value; // Assign new value for the image url
+
+        }
+      })
+      .catch( e => console.log(e));
+  }
+
+});
+
+// Image load
+image.addEventListener('load', function () {
+
+  resetValues();
+
+  let rect = image.getBoundingClientRect();
+
+  // Set height or width based on the aspect ratio
+  if (rect.width / rect.height > 1) {
+    currentHeight.value = 228;
+  } else {
+    currentWidth.value = 228;
+  }
+
+  rect = image.getBoundingClientRect();
+
+  baseWidth = currentWidth.value  = rect.width;
+  baseHeight = currentHeight.value = rect.height;
+
+  minVals = getMinVals();
+});
+
+// Beginning of the image dragging event
+image.addEventListener('mousedown', function(event) {
+
+  event.preventDefault(); // Prevent Firefox image dragging
+  isImageEvent = true;
+  startPosition = getMousePosition(event); // Assign initial mouse position
+
+}, true);
+
+// Beginning of the slider dragging event
+slider.addEventListener('mousedown', function () {
+
+  isSliderEvent = true;
+  sliderStart = slider.value;
+
+});
+
+// End of the mouse events. Reset
+document.addEventListener('mouseup', function() {
+
+  isImageEvent = false;
+  isSliderEvent = false;
+
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+
+  if (isImageEvent) {
+    dragImage(event);
+  }
+
+  if (isSliderEvent) {
+    zoom();
+  }
+
+}, true);
+
+
+
+function resetValues () {
+
+  slider.value = 100;
+  currentX.value = 178;
+  currentY.value = 178;
+
+}
+
+function getMousePosition (event) {
+  return { x : event.clientX, y : event.clientY };
+}
+
+function getMinVals () {
+  return { x: 178 - (currentWidth.value - 228), y: 178 - (currentHeight.value - 228) };
+}
+
+function getAxisValue (axisName) {
+  return image[axisName].baseVal.value + ( mousePosition[axisName] - startPosition[axisName] ) * 1.2;
+}
+
+function zoom () {
+
+  currentHeight.value  = baseHeight *  +slider.value / 100;
+  currentWidth.value   = baseWidth  *  +slider.value / 100;
+
+  minVals = getMinVals();
+
+  if (currentX.value < minVals.x){
+    currentX.value = minVals.x
+  }
+
+  if (currentY.value < minVals.y){
+    currentY.value = minVals.y
+  }
+
+}
+
+
+function dragImage (event) {
+
+  mousePosition = getMousePosition(event);
+
+  imageX = getAxisValue('x');
+  imageY = getAxisValue('y');
+
+  if (imageX >= minVals.x && imageX <= maxVals.x) currentX.value = imageX;
+  if (imageY >= minVals.y && imageY <= maxVals.y) currentY.value = imageY;
+
+  startPosition = mousePosition;
+
+}
